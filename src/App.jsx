8937be3ae9e45856df2e7d7a5eb9e51f74ec0592
omriki55 <omrigonen5050@gmail.com>
2026-03-07 20:16:07 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+          <div>
+                <h1>Family Chores</h1>h1>
+                <p>Welcome to the family chores app!</p>p>
+          </div>div>
+        )
+}
+
+export default App</div>
