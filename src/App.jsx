ad39e59a6ff3a060af71f99206aa81eafcfb6962
omriki55 <omrigonen5050@gmail.com>
2026-03-07 20:18:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import React from 'react'
+
 function App() {
-    return (
-          <div>
-                <h1>Family Chores</h1>h1>
-                <p>Welcome to the family chores app!</p>p>
-          </div>div>
-        )
+      return (
+              <div>
+                    <h1>Family Chores</h1>h1>
+                    <p>Welcome to the family chores app!</p>p>
+              </div>div>
+            )
 }
 
 export default App</div>
