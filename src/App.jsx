88f3e712d2043628443fb88ab41bac7db76d7940
omriki55 <omrigonen5050@gmail.com>
2026-03-07 20:25:34 +0200

    Fix App.jsx - remove duplicated closing tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 function App() {
-      return (
-              <div>
-                    <h1>Family Chores</h1>h1>
-                    <p>Welcome to the family chores app!</p>p>
-              </div>div>
-            )
+  return (
+    <div>
+      <h1>Family Chores</h1>
+      <p>Welcome to the family chores app!</p>
+    </div>
+  )
 }
 
-export default App</div>
+export default App
